f2f5edd6930d92c977c9e8dbec29b311252ec845
[callmehj] <[1258811671@qq.com]>
2019-10-24 13:53:43 +0800

    队列

---
+++ src/Queue/Main.java
@@ -0,0 +1,21 @@
+package Queue;
+
+
+public class Main {
+    public static void main(String[] args) {
+        MyQueue q = new MyQueue();
+        q.enQueue(5);
+        q.enQueue(3);
+        if (q.isEmpty() == false) {
+            System.out.println(q.Front());
+        }
+        q.deQueue();
+        if (q.isEmpty() == false) {
+            System.out.println(q.Front());
+        }
+        q.deQueue();
+        if (q.isEmpty() == false) {
+            System.out.println(q.Front());
+        }
+    }
+}
